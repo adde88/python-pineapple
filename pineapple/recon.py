eb6834970be527fb4961d4309df0b7b471d373b4
Mike Piekarski <mjpiekarski@gmail.com>
2018-09-13 10:45:42 -0400

    Rewrote the bulk of recon module for latest firmware. Added comments to methods. Need to rewrite pineapple module next

--- pineapple/recon.py
+++ pineapple/recon.py
@@ -4,6 +4,52 @@ class Recon(Module):
     def __init__(self, api):
         super(Recon, self).__init__(api, 'Recon')
     def getScanStatus(self, scanId, scanType):
+        """ This method is deprecated but left in for compatibility with older firmware """
         return self.request('getScanStatus', {'scan': {'scanID': scanId, 'scanType': scanType}})
     def startScan(self, scanType, scanDuration):
+        """ This method is deprecated but left in for compatibility with older firmware """
         return self.request('startScan', {'scanType': scanType, 'scanDuration': scanDuration})
+    def startNormalScan(self, scanType, scanDuration):
+        """ Start a Normal Scan
+            Scan Type: 
+                0 - 2.4 Ghz
+                1 - 5 Ghz
+                2 - Both
+        """
+    def startLiveScan(self, scanType, scanDuration):
+        """ Start a Live Scan
+            Scan Type:
+                0 - 2.4 Ghz
+                1 - 5 Ghz
+                2 - Both
+        """
+        return self.request('startNormalScan', { 'scanType': scanType, 'scanDuration': scanDuration})
+    def checkPineAPDaemon(self):
+        """ Errors on return but added to mirror recon api """
+        return self.request('checkPineAPDaemon')
+    def startPineAPDaemon(self):
+        """ Start Pine AP Daemon """
+        return self.request('startPineAPDaemon')
+    def getScans(self):
+        """ Return a list of Scans """
+        return self.request('getScans')
+    def downloadResults(self, scanId):
+        """ Download Scan Results by ID 
+                Basically a limited stub right now,
+                Pending rewrite of Pineapple module to download data
+        """
+        return self.request('downloadResults', { 'scanId': scanId })
+    def removeScan(self, scanId):
+        """ Delete a scan by ID """
+        return self.request('removeScan', { 'scanId': scanId})
+    def getCurrentScanID(self):
+        """ Return ID of Current Scan """
+        return self.request('getCurrentScanID')
+    def stopScan(self):
+        """ Stop currently running scan 
+                Note: This is not by scan ID but rather just currently running scan
+        """
+        return self.request('stopScan')
+    def loadResults(self, scanId):
+        return self.request('loadResults', { 'scanId': scanId })
+
