52aa06ddd3cac7617e0f7a4ab6f8a7564f0b3dda
Mike Piekarski <mjpiekarski@gmail.com>
2018-09-24 13:08:58 -0400

    some comments and a bug fixed in networking

--- pineapple/networking.py
+++ pineapple/networking.py
@@ -4,6 +4,9 @@ class Networking(Module):
     def __init__(self, api):
         super(Networking, self).__init__(api, 'Networking')
     def getRoutingTable(self):
+        """
+            Return Routing Table
+        """
         return self.request('getRoutingTable')
     def restartDNS(self):
         return self.request('restartDNS')
@@ -16,6 +19,9 @@ class Networking(Module):
     def resetWirelessConfig(self):
         return self.request('resetWirelessConfig')
     def getInterfaceList(self):
+        """
+            Return List of Interfaces
+        """
         return self.request('getInterfaceList')
     def saveAPConfig(self, apConfig):
         return self.request('saveAPConfig', {'apConfig': apConfig})
@@ -26,15 +32,21 @@ class Networking(Module):
     def setMac(self, mac):
         return self.request('setMac', {'mac': mac})
     def setRandomMac(self):
+        """
+            Set a random mac
+        """
         return self.request('setRandomMac')
     def resetMac(self):
         return self.request('resetMac')
     def scanForNetworks(self, interface):
         return self.request('scanForNetworks', {'interface': interface})
     def getClientInterfaces(self):
+        """
+            Return list of Client Interfaces
+        """
         return self.request('getClientInterfaces')
     def connectToAP(self, interface, security, password = ''):
-        return self.request('connectToAP', {'interface': interface, security: security, key: password})
+        return self.request('connectToAP', {'interface': interface, 'security': security, 'key': password})
     def checkConnection(self):
         return self.request('checkConnection')
     def disconnect(self, interface):
