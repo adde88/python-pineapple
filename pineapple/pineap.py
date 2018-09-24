4040edfc762e5555d56f3ae3d6eb5acfbf7f7075
Mike Piekarski <mjpiekarski@gmail.com>
2018-09-24 13:23:54 -0400

    Updated some bugs in pineap. Fixed a couple bugs.

--- pineapple/pineap.py
+++ pineapple/pineap.py
@@ -4,26 +4,73 @@ from module import Module
 class Pineap(Module):
     def __init__(self, api):
         super(Pineap, self).__init__(api, 'PineAP')
+
     def getSSIDPool(self):
+        """
+            Return raw SSID Pool.
+
+            Note: It's just a \n terminated list
+        """
         return self.request('getPool')
+
     def clearPool(self):
+        """
+            Clear the SSID Pool
+        """
         return self.request('clearPool')
+
     def addSSID(self, ssid):
+        """
+            Add SSID to Pool
+        """
         return self.request('addSSID', {'ssid': ssid})
+
     def removeSSID(self, ssid):
+        """
+            Remove SSID from Pool
+        """
         return self.request('removeSSID', {'ssid': ssid})
+
     def setPineAPSettings(self, settings):
+        """
+            Set PineAP Settings.
+            Hand settings as a Python Dictionary
+        """
         self.request('setPineAPSettings', {'settings': json.dumps(settings)})
+
     def getPineAPSettings(self):
+        """
+            Get Current PineAP Settings
+        """
         return self.request('getPineAPSettings')
+
     def deauth(self, sta, clients, multiplier, channel):
+        """
+            Deauth a Client from a Station X times on Y Channel
+        
+            Args in order: Station, Client, Multiplier, Channel
+
+        """
         multiplier = 10 if multiplier > 10 else multiplier
-        return self.request('deauth', {'sta': sta, 'clients': clients, 'multiplier': multiplier, channel: channel})
+        return self.request('deauth', {'sta': sta, 'clients': clients, 'multiplier': multiplier, 'channel': channel})
+
     def enable(self):
+        """
+            Enable PineAP
+        """
         return self.request('enable')
+
     def disable(self):
+        """
+            Disable PineAP
+        """
         return self.request('disable')
+
     def saveSettignsAsDefault(self, config = None):
+        """
+            Hand a config as Python Dictionary.
+            Once it updates, save as default
+        """
         if config:
             resp = self.setPineAPSettings(config)
             if (resp['error']):
