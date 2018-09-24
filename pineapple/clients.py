e985e210c486f540bafe99cb5695fe54d40c100e
Mike Piekarski <mjpiekarski@gmail.com>
2018-09-24 12:44:17 -0400

    Added some default to clients. Added missing methods to logging with some detail about what they do

--- pineapple/clients.py
+++ pineapple/clients.py
@@ -3,7 +3,16 @@ from module import Module
 class Clients(Module):
     def __init__(self, api):
         super(Clients, self).__init__(api, 'Clients')
+
     def getClientData(self):
+        """
+            Return List of Clients
+        """
         return self.request('getClientData')
+
     def kickClient(self, mac):
+        """
+            Kick a client by mac address
+        """
         return self.request('kickClient', {'mac': mac})
+
--- pineapple/logging.py
+++ pineapple/logging.py
@@ -1,15 +1,59 @@
 from module import Module
 
 class Logging(Module):
+
     def __init__(self, api):
         super(Logging, self).__init__(api, 'Logging')
+
     def getSyslog(self):
+        """
+            Return raw syslog
+        """
         return self.request('getSyslog')
+
     def getDmesg(self):
+        """
+            Return raw dmesg
+        """
         return self.request('getDmesg')
+
     def getReportingLog(self):
+        """
+            Return raw reporting log
+        """
         return self.request('getReportingLog')
+
     def getPineapLog(self):
+        """
+            Return PineAP Log in JSON Format
+        """
         return self.request('getPineapLog')
+
     def clearPineapLog(self):
+        """
+            Clear PineAP Log
+        """
         return self.request('clearPineapLog')
+
+    def getPineapLogLocation(self):
+        """
+            Return the path where PineAP Log is written
+    
+            default: /tmp/
+        """
+        return self.request('getPineapLogLocation')
+
+    def setPineapLogLocation(self, location):
+        """
+            Update the path where PineAP Log is written
+
+            default: /tmp/
+        """
+        return self.request('setPineapLogLocation', { 'location': location })
+    
+    def downloadPineapLog(self):
+        """
+            Return the download token for PineAP Log
+        """
+        return self.request('downloadPineapLog')
+
