fb3947d864d20d3bb5b2219d3e50cba80c0c8325
Mike Piekarski <mjpiekarski@gmail.com>
2018-09-24 13:05:20 -0400

    Updated filters. Added detail.

--- pineapple/filters.py
+++ pineapple/filters.py
@@ -3,21 +3,81 @@ from module import Module
 class Filters(Module):
     def __init__(self, api):
         super(Filters, self).__init__(api, 'Filters')
-    def getClientData(self):
-        return self.request('getClientData')
-    def getSSIDData(self):
+
+    def getSSIDMode(self):
+        """
+            Return SSID Mode
+                Allow or Deny
+        """
         return self.request('getSSIDData')
-    def toggleClientMode(self, allow):
-        mode = 'Allow' if enabled else 'Disallow'
+
+    def getClientMode(self):
+        """
+            Return Client Mode
+                Allow or Deny
+        """
+        return self.request('getClientMode')
+
+    def getSSIDFilters(self):
+        """
+            Return SSID Filters from DB
+        """
+        return self.request('getSSIDFilters')
+
+    def getClientFilters(self):
+        """
+            Return Client Filters from DB
+        """
+        return self.request('getClientFilters')
+
+    def getClientData(self):
+        """
+            Return array of mode and clientFilters
+        """
+        return self.request('getClientData')
+
+    def getSSIDData(self):
+        """
+            Return array of mode and ssidFilters
+        """
+        return self.request('getSSIDData')
+
+    def toggleClientMode(self, mode):
+        """
+            Toggle Client Mode
+                Allow or Deny are valid values.
+                Case Sensitive
+        """
         return self.request('toggleClientMode', {'mode': mode})
+
     def toggleSSIDMode(self, allow):
-        mode = 'Allow' if enabled else 'Disallow'
+        """
+            Toggle SSID Mode
+                Allow or Deny are valid values
+                Case Sensisitve
+        """
         return self.request('toggleSSIDMode', {'mode': mode})
+
     def addClient(self, mac):
+        """
+            Add Client by Mac to Filters
+        """
         return self.request('addClient', {'mac': mac})
+
     def removeClient(self, mac):
+        """
+            Remove Client by Mac from Filters
+        """
         return self.request('removeClient', {'mac': mac})
+
     def addSSID(self, ssid):
+        """
+            Add SSID to Filters
+        """
         return self.request('addSSID', {'ssid': ssid})
+
     def removeSSID(self, ssid):
+        """
+            Remove SSID from Filters
+        """
         return self.request('removeSSID', {'ssid': ssid})
--- pineapple/tracking.py
+++ pineapple/tracking.py
@@ -3,15 +3,54 @@ from module import Module
 class Tracking(Module):
     def __init__(self, api):
         super(Tracking, self).__init__(api, 'Tracking')
+
     def getScript(self):
+        """
+            Return contents of user tracking script.
+
+            Default Script contents:
+
+            #!/bin/bash
+
+            echo $MAC
+            echo $TYPE # 0 PROBE; 1 ASSOCIATION
+            echo $SSID
+        """
         return self.request('getScript')
+
     def setScript(self, script):
+        """
+            Set script content for user tracking
+
+            Default Script contents:
+
+            #!/bin/bash
+            echo $MAC
+            echo $TYPE # 0 PROBE; 1 ASSOCIATION
+            echo $SSID
+        """
         return self.request('saveScript', {'trackingScript': script})
+
     def getTrackingList(self):
+        """
+            Return list of macs being tracked
+        """
         return self.request('getTrackingList')
+
     def addMac(self, mac):
+        """
+            Add mac to Tracking List
+        """
         return self.request('addMac', {'mac': mac})
+
     def removeMac(self, mac):
+        """
+            Remove mac from tracking list
+        """
         return self.request('removeMac', {'mac': mac})
+
     def clearMacs(self):
+        """
+            Clear all macs from Tracking
+        """
         return self.request('clearMacs')
